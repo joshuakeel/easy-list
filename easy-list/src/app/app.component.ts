baf0d43a23dbb03b939bebcf948d356380067d29
Joshua Keel <joshuakeel@gmail.com>
2021-10-24 08:42:27 -0400

    Added basic in-memory list add/delete functionality.

--- easy-list/src/app/app.component.ts
+++ easy-list/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'easy-list';
+  title = 'Easy List';
+  item = '';
+  items = ['dog food', 'light bulbs', 'mac and cheese'];
+
+  onAddItem(item: string) {
+    this.items.push(item);
+    this.item = '';
+  }
+
+  deleteItem(item: string) {
+    const itemIndex = this.items.indexOf(item);
+    this.items.splice(itemIndex, 1);
+  }
 }
